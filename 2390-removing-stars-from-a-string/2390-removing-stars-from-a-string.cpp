7244a63fc061685549393657ee04cbde17702416
Ankit Kumar Mishra <kumarmishraankit18@gmail.com>
2022-08-28 11:51:05 +0530

    Time: 152 ms (16.67%), Space: 25.8 MB (16.67%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeStars(string s) {
+       string ans;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='*')
+            {
+                if(ans.size())
+                    ans.pop_back();
+            }
+            else ans.push_back(s[i]);
+        }
+        
+        return ans;
+    }
+};
+
